2046ad89e83c22fa02bb3098da3317d37fada3ad
evenage <charue.vyago2019@gmail.com>
2025-03-14 00:15:14 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, from routes!");
 });
 
 router.use("/contacts", require("./contacts"));
